b680cd11e90a8d7d59ef7faa8d6fda5f2494d58f
Gurur Kısaoğlu <kisaoglugurur@gmail.com>
2025-06-12 10:07:06 +0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Gurur on 12.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
